668cd2b90b67907adfce400b158d8b4496f74d3f
vroed <vrnkrd@gmail.com>
2024-02-08 11:38:56 +0100

    Added Spring Boot

---
+++ src/test/java/hello/HelloApplicationTests.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
